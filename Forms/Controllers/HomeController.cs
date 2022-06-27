c0366e26f562250912b86de19d769f2ef08fa8e5
GreedNeSS <ruslan199126@gmail.com>
2022-06-27 18:24:50 +0300

    Added form example.

---
+++ Forms/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Forms.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name, int age, string[] languages)
+        {
+            return Json(new { Name = name, Age = age, Languages = new List<string>(languages)});
+        }
+    }
+}
---
+++ Forms/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Forms.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
