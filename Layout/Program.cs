7410bc788f173c87b571e550bbce442d003ccd35
GreedNeSS <ruslan199126@gmail.com>
2022-06-29 18:01:16 +0300

    Fixed route

--- Layout/Program.cs
+++ Layout/Program.cs
@@ -3,6 +3,5 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
-app.MapControllerRoute("about", "{controller=Home}/{action=About}/{Id?}");
 
 app.Run();
