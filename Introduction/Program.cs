571c7e1d33389fa4dd4d8d29801c6fc400f5dbc4
GreedNeSS <ruslan199126@gmail.com>
2022-06-29 18:00:16 +0300

    Fix route.

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -3,6 +3,5 @@ builder.Services.AddMvc();
 var app = builder.Build();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
-app.MapControllerRoute("about", "{controller=Home}/{action=About}/{Id?}");
 
 app.Run();
